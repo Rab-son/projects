4744372df7a60b527beaf0c5b472555084f0b1c8
Rab-son <rabsonsayendajnr@gmail.com>
2023-01-25 09:11:36 +0200

    context provider

--- react-laravel/react/src/components/GuestLayout.jsx
+++ react-laravel/react/src/components/GuestLayout.jsx
@@ -1,6 +1,14 @@
 import { Outlet } from "react-router-dom";
+import {createBrowserRouter, Navigate} from 'react-router-dom';
+import { useStateContext } from "../contexts/ContextProvider";
 
 export function GuestLayout(){
+
+    const {token} = useStateContext()
+    if(token){
+        return <Navigate to={"/"} />
+    }
+
     return(
         <div>
             <Outlet/>

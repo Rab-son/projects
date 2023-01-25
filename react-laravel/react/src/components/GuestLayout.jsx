2e8fc41f99351a959edab6e532bf357ac4017505
Rab-son <rabsonsayendajnr@gmail.com>
2023-01-25 08:40:09 +0200

    layouts

---
+++ react-laravel/react/src/components/GuestLayout.jsx
@@ -0,0 +1,8 @@
+export function GuestLayout(){
+    return(
+        <>
+        
+        
+        </>
+    )
+}
--- react-laravel/react/src/router.jsx
+++ react-laravel/react/src/router.jsx
@@ -1,4 +1,6 @@
 import {createBrowserRouter} from 'react-router-dom';
+import { DefaultLayout } from './components/DefaultLayout';
+import { GuestLayout } from './components/GuestLayout';
 import Login from './views/Login';
 import NotFound from './views/NotFound';
 import Signup from './views/Signup';
@@ -6,20 +8,33 @@ import Users from './views/Users';
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login />
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/users',
+                element: <Users />
+            },
+        ]
     },
+
     {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
-    },
-    {
-        path: '*',
-        element: <NotFound />
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+            {
+                path: '*',
+                element: <NotFound />
+            },
+        ]
     },
 
 

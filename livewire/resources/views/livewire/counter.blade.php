ff3abb947b126a25ca2fb967c8ae11a5b5c1edb5
Rab-son <rabsonsayendajnr@gmail.com>
2023-01-31 22:55:58 +0200

    button css

--- livewire/resources/views/livewire/counter.blade.php
+++ livewire/resources/views/livewire/counter.blade.php
@@ -1,8 +1,12 @@
 <div>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
 
-    <button wire:click="increment">+</button>
+    <button wire:click="increment" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">
+        +
+    </button>
     <span>{{$count}}</span>
-    <button wire:click="decrement">-</button>
+    <button wire:click="decrement" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">
+        -
+    </button>
 
 </div>

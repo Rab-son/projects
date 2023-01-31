ac27709db2173bb27b8a234091d8c7add0e982af
Rab-son <rabsonsayendajnr@gmail.com>
2023-01-31 22:57:07 +0200

    centering

--- livewire/resources/views/livewire/counter.blade.php
+++ livewire/resources/views/livewire/counter.blade.php
@@ -1,12 +1,12 @@
 <div>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-
-    <button wire:click="increment" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">
-        +
-    </button>
-    <span>{{$count}}</span>
-    <button wire:click="decrement" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">
-        -
-    </button>
-
+    <div class="p-16 flex justify-center gap-6 items-center">
+        <button wire:click="increment" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">
+            +
+        </button>
+        <span>{{$count}}</span>
+        <button wire:click="decrement" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">
+            -
+        </button>
+    </div>
 </div>

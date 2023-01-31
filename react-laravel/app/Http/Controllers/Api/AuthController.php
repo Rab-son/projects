14d0daea5323554b0797e02769507e1cf998ce91
Rab-son <rabsonsayendajnr@gmail.com>
2023-01-31 21:25:31 +0200

    login

--- react-laravel/app/Http/Controllers/Api/AuthController.php
+++ react-laravel/app/Http/Controllers/Api/AuthController.php
@@ -15,37 +15,37 @@ class AuthController extends Controller
     public function signup(SignupRequest $request)
     {
         $data = $request->validated();
+        /** @var \App\Models\User $user */
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
 
-        $user->createToken('main')->plainTextToken;
-
-        return response(compact('user','token'));
-
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
     }
 
     public function login(LoginRequest $request)
     {
         $credentials = $request->validated();
-        if(!Auth:: attempt($credentials)){
+        if (!Auth::attempt($credentials)) {
             return response([
-                'message' => 'Provided email address or password is incorrect'
-            ]);
+                'message' => 'Provided email or password is incorrect'
+            ], 422);
         }
 
+        /** @var \App\Models\User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
-
+        return response(compact('user', 'token'));
     }
 
-    
-    public function logout()
+    public function logout(Request $request)
     {
+        /** @var \App\Models\User $user */
         $user = $request->user();
         $user->currentAccessToken()->delete();
-        return response ('', 204);
+        return response('', 204);
     }
 }

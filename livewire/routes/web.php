08dcabf9bc0336c47029bffe7707e8d5b2bc1710
Rab-son <rabsonsayendajnr@gmail.com>
2023-01-31 23:28:52 +0200

    todo list completed

--- livewire/routes/web.php
+++ livewire/routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route ;
 
 Route::get('/counter', function () {return view('welcome');})->name('counter');
 Route::get('/calculator', \App\Http\Livewire\Calculator::class)->name('calculator');
-/*
 Route::get('/todo-list', \App\Http\Livewire\TodoList::class)->name('todo-list');
+/*
+
 Route::get('/cascading-dropdown', \App\Http\Livewire\CascadingDropdown::class)->name('cascading-dropdown');
 Route::get('/products', \App\Http\Livewire\ProductsSearch::class)->name('products');
 Route::get('/image-upload', \App\Http\Livewire\ImageUpload::class)->name('image-upload');
